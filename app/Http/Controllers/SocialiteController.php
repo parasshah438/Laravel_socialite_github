757c3ad85fa15414350616d0c0c922e7c4b68ecc
Parasnshah <parasshah438@gmail.com>
2020-04-11 16:52:39 +0530

    uplaod socialite github code

---
+++ app/Http/Controllers/SocialiteController.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Http\Controllers;
+ use Illuminate\Http\Request;
+ use Validator,Redirect,Response,File;
+ use Socialite;
+ use App\User;
+ use Auth;
+
+ class SocialiteController extends Controller
+ {
+
+ public function index()
+ {
+     return view('login');
+ }
+ 
+ public function redirect($provider)
+ {
+     //return Socialite::driver($provider)->redirect();
+     return Socialite::driver($provider)->scopes(['email'])->redirect(); 
+ }
+
+ public function callback($provider)
+ {
+   $getInfo = Socialite::driver($provider)->user(); 
+   $user = $this->create($getInfo,$provider); 
+   Auth::login($user, true);
+   return redirect()->to('/home');
+ }
+
+ public function create($getInfo,$provider)
+ {
+   $user = User::where('email', $getInfo->email)->first();
+   if (!$user) {
+         $user = new User; 
+            $user['name'] = $getInfo->getName();
+            $user['email'] = $getInfo->getEmail();
+            $user['provider_id'] = $getInfo->id;
+            $user['provider'] = $provider;
+            $user['avatar'] = $getInfo->getAvatar();
+            $user['email_verified_at'] =  now();
+            $user->save();
+     }
+     return $user;
+  }
+}
